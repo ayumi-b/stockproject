a448822f6699f5796fc7707dc902d82d2d7cbf00
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-02-02 13:33:14 -0600

    added initial code to js file

---
+++ main.js
@@ -0,0 +1,47 @@
+/* global $: false, _: false */
+
+(function () {
+  'use strict';
+
+  var $input,
+      url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp/?symbol=';
+
+
+  
+  $(".target").submit( function(event){
+    var symbol = $(".symbol");
+    var symbolUrl = url + symbol + '&callback=?';
+
+    $.getJSON(url, function(data){
+    console.log(data);
+    });
+  });
+
+  function createTable(){
+    var docFragment = document.createDocumentFragment(); // contains all gathered nodes
+
+    var text = document.createTextNode(" NAME\nLAST PRICE\nCHANGE / CHANGE PERCENT\n");
+    docFragment.appendChild(text);
+
+    return docFragment;
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
